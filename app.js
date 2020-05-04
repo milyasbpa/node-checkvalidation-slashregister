38c9afbda2cfcbcb80d67482a87d8567f6e055e3
milyasbpa <basilyas.2015@gmail.com>
2020-05-04 15:52:27 +0700

    styling tengah

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const app = require ('express')();
+const express =require('express');
+const app = express();
 const path = require('path');
 const hbs = require('express-handlebars')
 
@@ -6,12 +7,15 @@ const hbs = require('express-handlebars')
 const routes= require('./routes/index')
 
 // views engine
-app.engine('hbs', hbs({extname: 'hbs',defaultLayout: 'index', layoutsDir: __dirname + '/views/'}));
+app.engine('hbs', hbs({extname: 'hbs',defaultLayout: 'layout', layoutsDir: __dirname + '/views/layout'}));
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','hbs')
 
 // pages
 app.use('/',routes)
+// untuk tahu template css ada dimana
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // listen server
 app.listen(1997,()=>{
