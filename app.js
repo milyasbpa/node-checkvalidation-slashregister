e824d594bfc08ba41c0402b805b0712570c206db
milyasbpa <basilyas.2015@gmail.com>
2020-05-05 12:03:36 +0700

    final validation

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express =require('express');
 const app = express();
 const path = require('path');
 const hbs = require('express-handlebars');
-const expressValidator = require('express-validator');
-const expressSession = require('express-session');
+// const expressValidator = require('express-validator');
+// const expressSession = require('express-session');
 
 
 
@@ -16,12 +16,12 @@ app.set('views',path.join(__dirname,'views'))
 app.set('view engine','hbs')
 
 // pages
-app.use('/',routes)
+app.use('/',routes);
 // untuk tahu template css ada dimana
 app.use(express.static(path.join(__dirname, 'public')));
 // validasi dan sesi
-app.use(expressSession);
-app.use(expressValidator);
+// app.use(expressSession);
+// app.use(expressValidator);
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 var bodyParser = require('body-parser');
+const {check, validationResult}=require('express-validator');
+const session = require('express-session')
 
 // bodyparser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
@@ -9,7 +11,27 @@ router.get('/',(req,res,next) => {
 })
 
 router.post('/sapa', urlencodedParser, function (req, res) {
+    console.log(req.body.sapaan);
     res.render('menyapa',{output:req.body.sapaan})
-  })
+})
+
+// register form dan validasi
+router.use(bodyParser.json())
+router.use(bodyParser.urlencoded({extended:false}))
+
+router.get('/register',(req,res,next) => {
+  res.render('register',{title: 'Register Page'});
+  // req.session.errors=null;
+})
+
+router.post('/register-submit', [check('email').isEmail(), check('password').equals('confirmPassword')], (req,res,next)=> {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    return res.render('register',{title: 'Register Page'})
+  } else {
+    return res.render('index', {title :'Homepage'})
+  }
+})
 
 module.exports=router
